303c3f335ef733e058d39022feb591bffee1e697
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-20 17:13:18 +0100

    Standalone: Shallow Testing

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,33 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { BookComponent } from '../book/book.component';
 import { Book } from '../shared/book';
-
 import { DashboardComponent } from './dashboard.component';
 
-// @Component({
-//   selector: 'br-book',
-//   template: ':-)',
-//   standalone: true
-// })
-// export class DummyBookComponent {
-
-//   @Input() book?: Book;
-
-//   @Output() rateUp = new EventEmitter<Book>();
-//   @Output() rateDown = new EventEmitter<Book>();
-// }
+@Component({
+  selector: 'br-book',
+  template: '😃',
+  standalone: true
+})
+export class DummyBookComponent {
+  @Input() book?: Book;
+}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-    imports: [DashboardComponent],
-    // declarations: [BookComponent]
-    })
-    .compileComponents();
+    await TestBed
+      .configureTestingModule({
+        imports: [DashboardComponent],
+      })
+      .overrideComponent(DashboardComponent, {
+        remove: { imports: [BookComponent] },
+        add: { imports: [DummyBookComponent] }
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
@@ -35,9 +35,6 @@ describe('DashboardComponent', () => {
   });
 
   it('should create', () => {
-    // expect(component).toBeTruthy();
-    // component.doRateDown({} as Book);
-    // component.doRateUp({} as Book);
-
+    expect(component).toBeTruthy();
   });
 });
