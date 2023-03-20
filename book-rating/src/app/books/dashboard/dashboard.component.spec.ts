986eafa1311228f77efd392af4b46acfdf99cc57
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-20 15:04:43 +0100

    TODO: how to mock DummyBookComponent ?!

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookComponent } from '../book/book.component';
+import { Book } from '../shared/book';
 
 import { DashboardComponent } from './dashboard.component';
+
+// @Component({
+//   selector: 'br-book',
+//   template: ':-)',
+//   standalone: true
+// })
+// export class DummyBookComponent {
+
+//   @Input() book?: Book;
+
+//   @Output() rateUp = new EventEmitter<Book>();
+//   @Output() rateDown = new EventEmitter<Book>();
+// }
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,8 +24,9 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [DashboardComponent]
-})
+    imports: [DashboardComponent],
+    // declarations: [BookComponent]
+    })
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
@@ -18,6 +35,9 @@ describe('DashboardComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    // expect(component).toBeTruthy();
+    // component.doRateDown({} as Book);
+    // component.doRateUp({} as Book);
+
   });
 });
