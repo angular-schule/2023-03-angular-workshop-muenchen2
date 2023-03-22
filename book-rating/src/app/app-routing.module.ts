5ea02fd2afa8a56ec4932da2537281b16a6cfdd1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-22 12:04:46 +0100

    router: config

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -6,7 +6,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false,
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
