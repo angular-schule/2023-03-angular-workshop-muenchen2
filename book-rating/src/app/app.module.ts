418236750a141d58553845bae6797e412877405b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-23 14:38:45 +0100

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools ng add @ngrx/schematics

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { ApiModule, Configuration } from './books/shared/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './books/book/book.component';
 
@@ -23,7 +26,10 @@ import { ApiModule, Configuration } from './books/shared/http';
     HttpClientModule,
     ApiModule.forRoot(() => new Configuration({
       basePath: 'https://api.angular.schule'
-    }))
+    })),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
