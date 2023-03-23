8740d85b0ee39f8dc25eea55b8a8ec21345e3d4b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-23 13:41:46 +0100

    rxjs: mergeMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { map, mergeMap } from 'rxjs';
+import { Book, BooksService, BooksslowService } from '../shared/http';
 
 @Component({
   selector: 'br-book-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent {
+
+  // bs = inject(BooksService);
+  bs = inject(BooksslowService);
+
+  book$ = inject(ActivatedRoute).paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || ''),
+    mergeMap(isbn => this.bs.booksIsbnSlowGet(isbn))
+  );
+
+  // book?: Book;
+  // constructor(route: ActivatedRoute) {
+  //   route.paramMap.pipe(
+  //     map(paramMap => paramMap.get('isbn') || ''),
+  //     map(isbn => this.bs.booksIsbnSlowGet(isbn))
+  //   ).subscribe(book$ => book$.subscribe(book => this.book = book))
+  // }
 
 }
